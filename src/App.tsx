301ada781b7632b4c4e77516815094f799bb3303
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-05-08 16:57:39 +0200

    init tauri

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./MovieForm";
+import UploadForm from "./MovieForm";
+
+
+function App() {
+
+
+  return (
+    <main>
+      <UploadForm></UploadForm>
+    </main>
+  );
+}
+
+export default App;
